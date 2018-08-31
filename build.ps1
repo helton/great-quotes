afd679fae64ef15f14f57d688664340533c10e8d
Sírius Roberto da Costa Gomes <sirius@ciandt.com>
2018-08-31 17:00:03 -0300

    Adding initialization, build and test scripts. Modifying mysql host name on hibernate.cfg.xml. Adding healthcheck param on docker-compose.

--- build.ps1
+++ build.ps1
@@ -4,10 +4,12 @@ echo "Removing all docker containers"
 docker rm $(docker ps -aq)
 echo "Removing all docker volumes"
 docker volume rm $(docker volume ls -q)
+cd backend
 echo "Booting docker-compose test file"
 docker-compose --file .\src\test\resources\docker-compose.yml up -d
 sleep 10
 echo "Starting build and tests"
-mvn clean test
+mvn package
 echo "Stopping all running docker containers"
 docker stop $(docker ps -aq)
+cd ..
