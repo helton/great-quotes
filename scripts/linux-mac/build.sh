08abb1e4f15ee01d9da3073c8d1727befb31e94a
Helton Carlos de Souza <heltoncarlossouza@gmail.com>
2018-09-04 23:42:33 -0300

    Refatorado frontend - Incluido opcao de visualizar great quote

--- scripts/linux-mac/build.sh
+++ scripts/linux-mac/build.sh
@@ -6,13 +6,11 @@ cd ../api
 echo "Booting docker-compose test file"
 docker-compose --file src/test/resources/docker-compose.yml up -d
 
-echo "Starting build api"
-# mvn clean package -Dmaven.test.skip=true
-docker run -it --rm --name great-quotes-api -v $(PWD):/usr/src/app -w /usr/src/app maven:3.5.4-jdk-8 mvn clean package -Dmaven.test.skip=true
+echo "Starting build API"
+docker run -it --rm --name great-quotes-api -v $(PWD):/usr/src/app -v ~/.m2:/root/.m2 -w /usr/src/app maven:3.5.4-jdk-8 mvn clean package -Dmaven.test.skip=true
 
 echo "Starting build app"
 cd ../app
-# npm install
 docker run -it --rm --name great-quotes-app -v $(PWD):/usr/src/app -w /usr/src/app node:10 npm install
 
 echo "Stopping all running docker containers"
--- scripts/windows/build.ps1
+++ scripts/windows/build.ps1
@@ -4,13 +4,11 @@ Set-Location ..\api
 Write-Output "Booting docker-compose test file"
 docker-compose --file .\src\test\resources\docker-compose.yml up -d
 
-Write-Output "Starting build and tests"
-# mvn clean package '-Dmaven.test.skip=true'
+Write-Output "Starting build API"
 docker run -it --rm --name great-quotes-api -v ${PWD}:/usr/src/app -v $env:USERPROFILE\.m2:/root/.m2 -w /usr/src/app maven:3.5.4-jdk-8 mvn clean package '-Dmaven.test.skip=true'
 
 Write-Output "Starting build app"
 Set-Location ..\app
-# npm install
 docker run -it --rm --name great-quotes-app -v ${PWD}:/usr/src/app -w /usr/src/app node:10 npm install
 
 Write-Output "Stopping all running docker containers"
